58609980836c1110b1b28a6b543fe80d60dfabff
@rharison <rharison.abreu@gmail.com>
2022-10-31 18:34:41 -0300

    F

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// import express from 'express';
 
-// (async () => {
-//   const app = express();
-//   const port = 5555;
-
-//   app.get('/', (req, res) => {
-//     res.send('Hello World!');
-//   });
-
-//   app.listen(port, () => {
-//     console.log(`Server http listening at http://localhost:${port}`);
-//   });
-// })()
 
 console.log('JAMEEEEEEEEL')
